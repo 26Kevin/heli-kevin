a3b7f1deea316034e71e191c3a6ce643bbf3b4ed
26Kevin <113374266+26Kevin@users.noreply.github.com>
2023-01-17 11:23:43 -0700

    updating

--- main.js
+++ main.js
@@ -11,7 +11,33 @@ let heliImg = document.createElement("img");
 heliImg.src = "img/heliBlueTransparent.png";
 
 let state = "start";
-
+let mouseIsPressed = false;
+let heli = {
+  x: 200,
+  y: 250,
+  w: 80,
+  h: 40,
+  speed: 0,
+  accel: 0.7
+}
+let wall1 = {
+  x: cnv.width,
+  y: Math.random() * 300 + 100,
+  w: 50,
+  h: 100
+}
+let wall2 = {
+  x: cnv.width + 500,
+  y: Math.random() * 300 + 100,
+  w: 50,
+  h: 100
+}
+let wall3 = {
+  x: cnv.width + 1000,
+  y: Math.random() * 300 + 100,
+  w: 50,
+  h: 100
+}
 // Draw Function
 window.addEventListener("load", draw);
 
@@ -19,7 +45,7 @@ function draw() {
   if (state === "start") {
     drawStart();
   } else if (state === "gameon") {
-    drawGame();
+    runGame();
   } else if (state === "gameover") {
     drawGameOver();
   }
@@ -30,9 +56,17 @@ function draw() {
 
 // EVENT STUFF
 document.addEventListener("mousedown", mousedownHandler);
+document.addEventListener("mouseup", mouseupHandler);
 
 function mousedownHandler() {
+  mouseIsPressed = true;
+
+  // Start Game on Mousedown
   if (state === "start") {
     state = "gameon";
   } 
+}
+
+function mouseupHandler () {
+  mouseIsPressed = false;
 }
